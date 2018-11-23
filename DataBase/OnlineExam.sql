935e1ee27b928a1b911062f63bd26ff5067b2a8d
unknown <sheahad@gmail.com>
2018-11-24 02:38:40 +0600

    Create OnlineExamDb

--- DataBase/OnlineExam.sql
+++ DataBase/OnlineExam.sql
@@ -1,3 +1,12 @@
 CREATE DATABASE OnlineExamDb
 
 USE OnlineExamDb
+
+CREATE TABLE organiaationTable(
+Id int IDENTITY (1,1) PRIMARY KEY,
+Name varchar(100),
+Code varchar(50),
+ConatactNo varchar(150),
+About nvarchar(MAX),
+Logo varchar(MAX)
+)
