24f66735f3e505673fa591a9aa011d6cb6bcacc6
Your New Name <sheahad@gmail.com>
2018-12-07 13:06:11 +0600

    Add LogIn

---
+++ OnlineExamProject/OnlineExam/OnlineExam.App/Models/ResultCreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineExam.App.Models
+{
+    public class ResultCreateViewModel
+    {
+    }
+}
