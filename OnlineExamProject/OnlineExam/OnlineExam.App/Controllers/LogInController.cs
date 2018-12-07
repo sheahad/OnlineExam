ed8d02f0a438665eed7887b51ac9303adbed9583
Your New Name <sheahad@gmail.com>
2018-12-07 15:14:35 +0600

    form authentication implementation

--- OnlineExamProject/OnlineExam/OnlineExam.App/Controllers/LogInController.cs
+++ OnlineExamProject/OnlineExam/OnlineExam.App/Controllers/LogInController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using OnlineExam.Models.Models;
 
 namespace OnlineExam.App.Controllers
 {
@@ -13,6 +15,22 @@ namespace OnlineExam.App.Controllers
         public ActionResult LogIn()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User model)
+        {
+            //if user is valid user
+            FormsAuthentication.SetAuthCookie(model.UserName,false);
+            return RedirectToAction("Index", "Home");
+
+        }
+
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
 	}
 }
--- OnlineExamProject/OnlineExam/OnlineExam.App/Controllers/OrganizationController.cs
+++ OnlineExamProject/OnlineExam/OnlineExam.App/Controllers/OrganizationController.cs
@@ -6,12 +6,21 @@ using System.Web.Mvc;
 
 namespace OnlineExam.App.Controllers
 {
+    [Authorize]
     public class OrganizationController : Controller
     {
         //
         // GET: /Organization/
         public ActionResult Index()
         {
+            return View();
+        }
+
+        
+        public ActionResult Save()
+        {
+            ViewBag.UserName = HttpContext.User.Identity.Name;
+            
             return View();
         }
 	}
